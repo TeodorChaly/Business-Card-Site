253c4e20c2ffcb49307694e76eac818fb1a50032
Teodor Calijs <teodorcalijs@MacBook-Pro.local>
2023-02-19 13:27:17 +0200

    Views and html added

--- mysite/buisness_card/urls.py
+++ mysite/buisness_card/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Project_view.as_view(),)
-
+    path("", views.Project_view.as_view(),),
+    path("<int:pk>/", views.Project_Detail.as_view())
 ]
--- mysite/buisness_card/views.py
+++ mysite/buisness_card/views.py
@@ -4,5 +4,10 @@ from .models import Project
 
 class Project_view(View):
     def get(self, request):
-        project = Project.objects.all()
-        return render(request, "posts/project_list.html", {"project_list":project})
+        projects = Project.objects.all()
+        return render(request, "posts/projects_list.html", {"project_list":projects})
+
+class Project_Detail(View):
+    def get(self, request, pk):
+        project = Project.objects.get(id=pk)
+        return render(request, "posts/project_info.html", {"project":project})
