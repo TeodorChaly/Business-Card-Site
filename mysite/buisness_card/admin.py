2c3737332ffd4bd5b7c617eabc327cbc20d43bca
Theodor <teodors.calijs@gmail.com>
2023-02-22 21:21:12 +0200

    Fixed problem with image and Admin functional adding

--- mysite/buisness_card/admin.py
+++ mysite/buisness_card/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 from .models import Project, QuestionsAnswer, Reviews, ProjectImages
 
+@admin.register(Project)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id","title","date", "url", "draft")
+    list_display_links = ("title",)
 
-admin.site.register(Project)
-admin.site.register(QuestionsAnswer)
-admin.site.register(Reviews)
-admin.site.register(ProjectImages)
+@admin.register(QuestionsAnswer)
+class QuestionsAnswerAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+    list_display_links = ("id",)
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+    list_display_links = ("id",)
+
+@admin.register(ProjectImages)
+class ProjectImagesAdmin(admin.ModelAdmin):
+    list_display = ("id","title", "movie")
+    list_display_links = ("id","title", "movie")
+
+# admin.site.register(Project, CategoryAdmin)
+# admin.site.register(QuestionsAnswer)
+# admin.site.register(Reviews)
+# admin.site.register(ProjectImages)
