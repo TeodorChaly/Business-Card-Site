8befe567ac127ceba4ffe7f93956db046ed59487
Teodor Calijs <teodorcalijs@MacBook-Pro.local>
2023-03-04 00:07:33 +0200

    Add registration form

--- mysite/registration/urls.py
+++ mysite/registration/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # ...
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
-    # ...
+    path('register/', registration_view, name='register'),
+
 ]
--- mysite/registration/views.py
+++ mysite/registration/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 
 def user_login(request):
     if request.method == 'POST':
@@ -17,3 +20,16 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return render(request, 'registration/logout.html')
+
+def registration_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully. Please log in.')
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'registration/registration.html', {'form': form})
+
